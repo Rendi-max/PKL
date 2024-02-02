726670655c0daf829fe71d67a16287c4e70ecd07
Rendi-max <antihack696@gmail.com>
2024-02-02 14:12:41 +0700

    update portofolio

--- Portofolio/components/about.vue
+++ Portofolio/components/about.vue
@@ -2,12 +2,12 @@
     <div  id="about" class="d-flex flex-wrap mt-10 lg:p-20 pb-16 border-b-2 border-yellow-400">
         <div id="about" class="lg:columns-2">
             <img src="../assets/alamak.jpg" alt="Rendi Firman Syah" class="scale-75 rounded-3xl lg:scale-75">
-            <div class="text-white mx-auto lg:mx-0 px-10 lg:px-0 lg:py-14" style="width: 500px;">
+            <div class="text-white mx-auto lg:mx-0 px-10 lg:px-0 lg:py-14 w-[330px] sm:w-[500px]">
                 <h2 class="text-white mb-6 text-3xl lg:text-5xl font-extrabold hover:text-purple-500  shadow-lg">About me</h2>
                 <div class="space-y-4 text-xs lg:text-lg">
                     <h3 class="text-white font-bold">Name: <span class="text-gray-500 ms-12 lg:ms-16">Rendi Firman Syah</span></h3>
                     <h3 class="text-white font-bold">Date of Birth: <span class="text-gray-500 ms-2 lg:ms-1">July 23, 2006</span></h3>
-                    <h3 class="text-white font-bold">Addres: <span class="text-gray-500 ms-10 lg:ms-14">Adipasir 07/02, Rakit, Banjarnegara</span></h3>
+                    <h3 class="text-white font-bold">Addres: <span class="text-gray-500 ms-10 lg:ms-14">Adipasir 07/02, Rakit, <span class="pl-2 ml-20 sm:p-0 sm:ml-0">Banjarnegara</span></span></h3>
                     <h3 class="text-white font-bold"><span class="mr-1">Email:</span> <span class="ms-11 text-gray-500  lg:ms-16">antihack696@gmil.com</span></h3>
                     <h3 class="text-white font-bold"><span class="mr-1">Phone:</span> <span class="ms-10 text-gray-500 lg:ms-14">085870603466</span></h3>
                 </div>
--- Portofolio/components/navbar.vue
+++ Portofolio/components/navbar.vue
@@ -1,6 +1,6 @@
 <template>
 <nav id="nav-menu" class="border-b-2 bg-black border-yellow-400 sticky top-0 z-50">
-  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+  <div class=" max-w-7xl sm:px-6 lg:px-8">
     <div class="relative flex h-16 items-center justify-between">
       <div class="flex flex-1 sm:items-stretch sm:justify-start">
         <div class="flex flex-shrink-0 items-center">
@@ -8,16 +8,15 @@
         </div>
         <div class="hidden sm:ml-6 sm:block">
           <div class="flex space-x-4">
-            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
             <a href="#" class="font-extrabold text-white rounded-md px-3 py-3 text-3xl" aria-current="page">Rendi</a>
            
           </div>
         </div>
       </div>
-      <div class=" pr-2 inset-y-0 right-0 justify-center d-flex items-center sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+      <div class=" pr-2  d-flex items-center sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <NuxtLink to="#home" class="text-white active:text-yellow-400 active:border-b-yellow-400 active:border-b-2 hover:border-b-yellow-400 hover:border-b-2 hover:text-yellow font-extrabold px-4 py-2 text-[11px] sm:text-xl">Home</NuxtLink>
             <NuxtLink to="#about" class="text-white active:text-yellow-400 active:border-b-yellow-400 active:border-b-2 hover:border-b-yellow-400 hover:border-b-2 hover:text-yellow font-extrabold px-4 py-2 text-[11px] sm:text-xl">About</NuxtLink>
-            <NuxtLink to="#resume" class="text-white active:text-yellow-400 active:border-b-yellow-400 active:border-b-2 hover:border-b-yellow-400 hover:border-b-2 hover:text-yellow font-extrabold px-4 py-2 text-[11px] sm:text-xl">Resume</NuxtLink>
+            <NuxtLink to="#resume" class="text-white active:text-yellow-400 active:border-b-yellow-400 active:border-b-2 hover:border-b-yellow-400 w-[70px] hover:border-b-2 hover:text-yellow font-extrabold px-4 py-2 text-[11px] sm:text-xl">Resume</NuxtLink>
             <NuxtLink to="#project" class="text-white active:text-yellow-400 active:border-b-yellow-400 active:border-b-2 hover:border-b-yellow-400 hover:border-b-2 hover:text-yellow font-extrabold px-4 py-2 text-[11px] sm:text-xl">Project</NuxtLink>
             <NuxtLink to="#contact" class="text-white active:text-yellow-400 active:border-b-yellow-400 active:border-b-2 hover:border-b-yellow-400 hover:border-b-2 hover:text-yellow font-extrabold px-4 py-2 text-[11px] sm:text-xl">Contact</NuxtLink>
       </div>
